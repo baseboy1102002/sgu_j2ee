560e682552f2e1a15a0742f0583c912c366e1062
Thanh <quocthanh056@gmail.com>
2023-11-07 09:01:12 +0700

    Update lại model
    Update lại model

---
+++ src/main/java/modelMapper/TuongTacBinhLuanMapper.java
@@ -0,0 +1,25 @@
+package modelMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.TuongTacBinhLuan;
+
+public class TuongTacBinhLuanMapper implements rowMapper<TuongTacBinhLuan> {
+	@Override
+	public TuongTacBinhLuan mapRow(ResultSet rs) {
+		// TODO Auto-generated method stub
+		TuongTacBinhLuan tuongTacBinhLuan = new TuongTacBinhLuan();
+		try {
+			tuongTacBinhLuan.setMaBinhLuan(rs.getInt("MaBinhLuan"));
+			tuongTacBinhLuan.setMaNguoiDung(rs.getInt("MaNguoiDung"));
+			tuongTacBinhLuan.setNgayGioTuongTac(rs.getDate("NgayGioTuongTac"));
+			tuongTacBinhLuan.setTrangThai(rs.getString("TrangThai"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+}
