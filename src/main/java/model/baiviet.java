99d445dca1d416dd7d2b7eee76c3d1354a8f2e50
ThisIsAmip <99376179+ThisIsAmip@users.noreply.github.com>
2023-11-08 08:21:08 +0700

    changes

--- src/main/java/model/baiviet.java
+++ src/main/java/model/baiviet.java
@@ -1,20 +1,19 @@
 package model;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
+public class BaiViet {
+	private int maBaiViet;
+	private Date ngayDang;
+	private String noiDung;
+	private String trangThai;
+	private int maNguoiDung;
 
-public class baiviet {
-	int maBaiViet;
-	Timestamp ngayDang;
-	String noiDung;
-	String trangThai;
-	int maNguoiDung;
-	
-	public baiviet() {
+	public BaiViet() {
 		super();
 	}
-	public baiviet(int maBaiViet, Timestamp ngayDang, String noiDung, String trangThai, int maNguoiDung) {
+
+	public BaiViet(int maBaiViet, Date ngayDang, String noiDung, String trangThai, int maNguoiDung) {
 		super();
 		this.maBaiViet = maBaiViet;
 		this.ngayDang = ngayDang;
@@ -22,36 +21,51 @@ public class baiviet {
 		this.trangThai = trangThai;
 		this.maNguoiDung = maNguoiDung;
 	}
+
 	public int getMaBaiViet() {
 		return maBaiViet;
 	}
+
 	public void setMaBaiViet(int maBaiViet) {
 		this.maBaiViet = maBaiViet;
 	}
-	public Timestamp getNgayDang() {
+
+	public Date getNgayDang() {
 		return ngayDang;
 	}
-	public void setNgayDang(Timestamp ngayDang) {
+
+	public void setNgayDang(Date ngayDang) {
 		this.ngayDang = ngayDang;
 	}
+
 	public String getNoiDung() {
 		return noiDung;
 	}
+
 	public void setNoiDung(String noiDung) {
 		this.noiDung = noiDung;
 	}
+
 	public String getTrangThai() {
 		return trangThai;
 	}
+
 	public void setTrangThai(String trangThai) {
 		this.trangThai = trangThai;
 	}
+
 	public int getMaNguoiDung() {
 		return maNguoiDung;
 	}
+
 	public void setMaNguoiDung(int maNguoiDung) {
 		this.maNguoiDung = maNguoiDung;
 	}
 
+	@Override
+	public String toString() {
+		return "BaiViet [maBaiViet=" + maBaiViet + ", ngayDang=" + ngayDang + ", noiDung=" + noiDung + ", trangThai="
+				+ trangThai + ", maNguoiDung=" + maNguoiDung + "]";
+	}
+
 }
-	
--- src/main/java/model/nguoidung.java
+++ src/main/java/model/nguoidung.java
@@ -2,110 +2,140 @@ package model;
 
 import java.util.Date;
 
-public class nguoidung {
-	private int MaNguoiDung;
-	private String Email;
-	private String HoVaTen;
-	private String MaQR;
-	private String HinhDaiDien;
-	private String SoDienThoai;
-	private Date NgaySinh;
-	private String TenTaiKhoan;
-	private String LoaiTaiKhoan;
-	private String MaXacNhan;
-	private String MatKhau;
-	private String TrangThai;
-	public nguoidung() {
+public class NguoiDung {
+	private int maNguoiDung;
+	private String email;
+	private String hoVaTen;
+	private String maQR;
+	private String hinhDaiDien;
+	private String soDienThoai;
+	private Date ngaySinh;
+	private String loaiTaiKhoan;
+	private String maXacNhan;
+	private String matKhau;
+	private String trangThai;
+
+	public NguoiDung() {
+		super();
 	}
 
-	public nguoidung(int maNguoiDung, String email, String hoVaTen, String maQR, String hinhDaiDien, String soDienThoai,
-			Date ngaySinh, String tenTaiKhoan, String loaiTaiKhoan, String maXacNhan, String matKhau,
-			String trangThai) {
+	public NguoiDung(int maNguoiDung, String hoVaTen, String hinhDaiDien) {
 		super();
-		MaNguoiDung = maNguoiDung;
-		Email = email;
-		HoVaTen = hoVaTen;
-		MaQR = maQR;
-		HinhDaiDien = hinhDaiDien;
-		SoDienThoai = soDienThoai;
-		NgaySinh = ngaySinh;
-		TenTaiKhoan = tenTaiKhoan;
-		LoaiTaiKhoan = loaiTaiKhoan;
-		MaXacNhan = maXacNhan;
-		MatKhau = matKhau;
-		TrangThai = trangThai;
+		this.maNguoiDung = maNguoiDung;
+		this.hoVaTen = hoVaTen;
+		this.hinhDaiDien = hinhDaiDien;
+	}
+
+	public NguoiDung(int maNguoiDung, String email, String hoVaTen, String maQR, String hinhDaiDien, String soDienThoai,
+			Date ngaySinh, String loaiTaiKhoan, String maXacNhan, String matKhau, String trangThai) {
+		super();
+		this.maNguoiDung = maNguoiDung;
+		this.email = email;
+		this.hoVaTen = hoVaTen;
+		this.maQR = maQR;
+		this.hinhDaiDien = hinhDaiDien;
+		this.soDienThoai = soDienThoai;
+		this.ngaySinh = ngaySinh;
+		this.loaiTaiKhoan = loaiTaiKhoan;
+		this.maXacNhan = maXacNhan;
+		this.matKhau = matKhau;
+		this.trangThai = trangThai;
 	}
 
 	public int getMaNguoiDung() {
-		return MaNguoiDung;
+		return maNguoiDung;
 	}
+
 	public void setMaNguoiDung(int maNguoiDung) {
-		MaNguoiDung = maNguoiDung;
+		this.maNguoiDung = maNguoiDung;
 	}
+
 	public String getEmail() {
-		return Email;
+		return email;
 	}
+
 	public void setEmail(String email) {
-		Email = email;
+		this.email = email;
 	}
+
 	public String getHoVaTen() {
-		return HoVaTen;
+		return hoVaTen;
 	}
+
 	public void setHoVaTen(String hoVaTen) {
-		HoVaTen = hoVaTen;
+		this.hoVaTen = hoVaTen;
 	}
+
 	public String getMaQR() {
-		return MaQR;
+		return maQR;
 	}
+
 	public void setMaQR(String maQR) {
-		MaQR = maQR;
+		this.maQR = maQR;
 	}
+
 	public String getHinhDaiDien() {
-		return HinhDaiDien;
+		return hinhDaiDien;
 	}
+
 	public void setHinhDaiDien(String hinhDaiDien) {
-		HinhDaiDien = hinhDaiDien;
+		this.hinhDaiDien = hinhDaiDien;
 	}
+
 	public String getSoDienThoai() {
-		return SoDienThoai;
+		return soDienThoai;
 	}
+
 	public void setSoDienThoai(String soDienThoai) {
-		SoDienThoai = soDienThoai;
+		this.soDienThoai = soDienThoai;
 	}
+
 	public Date getNgaySinh() {
-		return NgaySinh;
+		return ngaySinh;
 	}
+
 	public void setNgaySinh(Date ngaySinh) {
-		NgaySinh = ngaySinh;
+		this.ngaySinh = ngaySinh;
 	}
-	public String getTenTaiKhoan() {
-		return TenTaiKhoan;
-	}
-	public void setTenTaiKhoan(String tenTaiKhoan) {
-		TenTaiKhoan = tenTaiKhoan;
-	}
+
 	public String getLoaiTaiKhoan() {
-		return LoaiTaiKhoan;
+		return loaiTaiKhoan;
 	}
+
 	public void setLoaiTaiKhoan(String loaiTaiKhoan) {
-		LoaiTaiKhoan = loaiTaiKhoan;
+		this.loaiTaiKhoan = loaiTaiKhoan;
 	}
+
 	public String getMaXacNhan() {
-		return MaXacNhan;
+		return maXacNhan;
 	}
+
 	public void setMaXacNhan(String maXacNhan) {
-		MaXacNhan = maXacNhan;
+		this.maXacNhan = maXacNhan;
 	}
+
 	public String getMatKhau() {
-		return MatKhau;
+		return matKhau;
 	}
+
 	public void setMatKhau(String matKhau) {
-		MatKhau = matKhau;
+		this.matKhau = matKhau;
 	}
+
 	public String getTrangThai() {
-		return TrangThai;
+		return trangThai;
 	}
+
 	public void setTrangThai(String trangThai) {
-		TrangThai = trangThai;
+		this.trangThai = trangThai;
 	}
+
+	@Override
+	public String toString() {
+		return "NguoiDung [maNguoiDung=" + maNguoiDung + ", email=" + email + ", hoVaTen=" + hoVaTen + ", maQR=" + maQR
+				+ ", hinhDaiDien=" + hinhDaiDien + ", soDienThoai=" + soDienThoai + ", ngaySinh=" + ngaySinh
+				+ ", tenTaiKhoan=" + ", loaiTaiKhoan=" + loaiTaiKhoan + ", maXacNhan=" + maXacNhan + ", matKhau="
+				+ matKhau + ", trangThai=" + trangThai + "]";
+	}
+
 }
