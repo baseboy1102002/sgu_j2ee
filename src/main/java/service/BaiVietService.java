c842876222f8f9ee0851a92acac2d4301aeda4df
Thanh <quocthanh056@gmail.com>
2023-11-08 09:19:26 +0700

    thêm pop-up chỉnh sửa baiviet
    thêm pop-up chỉnh sửa baiviet

--- src/main/java/service/BaiVietService.java
+++ src/main/java/service/BaiVietService.java
@@ -25,6 +25,20 @@ public class BaiVietService extends DAOService<BaiViet> {
 		return update(sql, maBaiViet);
 		
 	}
+	public int saveBaiViet(BaiViet baiviet) {
+		String sql="insert into baiviet (NgayDang, NoiDung, TrangThai, MaNguoiDung)"
+				+ "values (?, ?, ?, ?)";
+		return insert(sql, baiviet.getNgayDang(), baiviet.getNoiDung(), baiviet.getTrangThai(), baiviet.getMaNguoiDung());
+	}
 	
+	public Boolean deleteBaiViet(int MaBaiViet) {
+		String sql = "update baiviet set TrangThai = 'No' where MaBaiViet = ?";
+		return update(sql, MaBaiViet);
+	}
+	
+	public Boolean updateBaiViet(String NoiDung, int MaBaiViet) {
+		String sql = "update baiviet set NoiDung = ? where MaBaiViet = ?";
+		return update(sql, NoiDung, MaBaiViet);
+	}
 	
 }
